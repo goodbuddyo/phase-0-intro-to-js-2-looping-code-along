f10bbaa66e19296d83f6071c6185fd356fda2ff6
Seano <sean01234@yahoo.com>
2023-04-01 19:00:16 -0400

    for and while loop tests

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.lenght; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(stringArray, eventName) {
+  let newArray = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    newArray.push(`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`)
+    console.log(i);
+  }
+  // console.log(newnpm testArray);
+  return newArray;
+}
+writeCards(names, "surprise");
+
+
+function countDown(positiveIntiger) {
+  // positiveIntiger = Math.abs(positiveIntiger);
+  while (positiveIntiger >= 0) {
+    console.log(positiveIntiger);
+    positiveIntiger--;
+  }
+}
+
+countDown(5);
